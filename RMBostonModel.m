79386afdedfb66166c1586e527537d870f22ba8f
Estheridota <72559849+Estheridota@users.noreply.github.com>
2020-10-08 09:00:27 -0700

    Add files via upload

---
+++ RMBostonModel.m
@@ -0,0 +1,29 @@
+
+fid = fopen('Boston Housing prices.txt', 'r');
+data = fscanf(fid, '%f');
+
+data = reshape(data(1:end), 14, 506).';
+
+CRIM = data(:, 1); ZN = data(:, 2); INDUS = data(:, 3); CHAS = data(:, 4); 
+NOX = data(:, 5); RM = data(:, 6); AGE = data(:, 7); DIS = data(:, 8); 
+RAD = data(:, 9); TAX = data(:, 10); PTRATIO = data(:, 11); B = data(:, 12); 
+LSTAT = data(:, 13); MEDV = data(:, 14); 
+
+figure; % open a new figure window
+
+plot(RM, MEDV, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('MEDV'); % Set the y-axis label
+xlabel('RM'); % Set the x-axis label
+
+X = [ones(506, 1), data(:,1)];
+
+theta = zeros(2, 1);
+theta(1) = -30.5;
+theta(2) = 8.5;
+
+% Plot the linear fit
+hold on; % keep previous plot visible
+plot(X(:,2), X*theta, '-')
+legend('Training data', 'Linear regression')
+hold off % don't overlay any more plots on this figure
+
